2a1c4962a022453dd6eaf05030eb3bb7c344059e
ChaerulHidayat17 <chaerulhidayat70@gmail.com>
2024-06-27 13:29:05 +0700

    Update Pergantian Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:airplane/cubit/page_cubit.dart';
 import 'package:airplane/ui/pages/bonus_page.dart';
 import 'package:airplane/ui/pages/get_started_page.dart';
 import 'package:airplane/ui/pages/main_page.dart';
 import 'package:airplane/ui/pages/sign_up_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'ui/pages/splash_page.dart';
 
 void main() => runApp(MyApp());
@@ -12,15 +14,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => SplashPage(),
-        '/get-started': (context) => GetStartedPage(),
-        '/sign-up': (context) => SignUpPage(),
-        '/bonus': (context) => BonusPage(),
-        '/main': (context) => MainPage(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => PageCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => SplashPage(),
+          '/get-started': (context) => GetStartedPage(),
+          '/sign-up': (context) => SignUpPage(),
+          '/bonus': (context) => BonusPage(),
+          '/main': (context) => MainPage(),
+        },
+      ),
     );
   }
 }
--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,9 +1,11 @@
+import 'package:airplane/cubit/page_cubit.dart';
 import 'package:airplane/ui/home_page.dart';
 import 'package:airplane/ui/pages/setting_page.dart';
 import 'package:airplane/ui/pages/transaction_page.dart';
 import 'package:airplane/ui/pages/wallet_page.dart';
 import 'package:airplane/ui/pages/widgets/custom_bottom_navigation_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../shared/theme.dart';
 
 class MainPage extends StatelessWidget {
@@ -11,8 +13,19 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget buildContent() {
-      return SettingPage();
+    Widget buildContent(int currentIndex) {
+      switch (currentIndex) {
+        case 0:
+          return HomePage();
+        case 1:
+          return TransactionPage();
+        case 2:
+          return WalletPage();
+        case 3:
+          return SettingPage();
+        default:
+          return HomePage();
+      }
     }
 
     Widget customBottomNavigation() {
@@ -31,16 +44,19 @@ class MainPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               CustomBottomNavigationItem(
+                index: 0,
                 imageUrl: 'assets/icon_home.png',
-                isSelected: true,
               ),
               CustomBottomNavigationItem(
+                index: 1,
                 imageUrl: 'assets/icon_booking.png',
               ),
               CustomBottomNavigationItem(
+                index: 2,
                 imageUrl: 'assets/icon_card.png',
               ),
               CustomBottomNavigationItem(
+                index: 3,
                 imageUrl: 'assets/icon_settings.png',
               ),
             ],
@@ -49,14 +65,18 @@ class MainPage extends StatelessWidget {
       );
     }
 
-    return Scaffold(
-      backgroundColor: kBackgroundColor,
-      body: Stack(
-        children: [
-          buildContent(),
-          customBottomNavigation(),
-        ],
-      ),
+    return BlocBuilder<PageCubit, int>(
+      builder: (context, currentIndex) {
+        return Scaffold(
+          backgroundColor: kBackgroundColor,
+          body: Stack(
+            children: [
+              buildContent(currentIndex),
+              customBottomNavigation(),
+            ],
+          ),
+        );
+      },
     );
   }
 }
